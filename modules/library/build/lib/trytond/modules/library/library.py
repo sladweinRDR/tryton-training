788602491a2e54bc34fac7ed0d4680d72d7cc4df
Samuel Ladwein <sladwein@roederer.fr>
2022-12-07 16:23:06 +0100

    feat(): Add all homework step4 entry point

--- modules/library/build/lib/trytond/modules/library/library.py
+++ modules/library/build/lib/trytond/modules/library/library.py
@@ -15,11 +15,16 @@ class Genre(ModelSQL, ModelView):
     'Genre'
     __name__ = 'library.genre'
 
+    name = fields.Char('Name', required=True)
+
 
 class Editor(ModelSQL, ModelView):
     'Editor'
     __name__ = 'library.editor'
 
+    name = fields.Char('Name', required=True)
+    creation_date = fields.Date('Creation date',
+        help='The date at which the editor was created')
     genres = fields.Many2Many('library.editor-library.genre', 'editor',
         'genre', 'Genres')
 
@@ -39,6 +44,10 @@ class Author(ModelSQL, ModelView):
     __name__ = 'library.author'
 
     books = fields.One2Many('library.book', 'author', 'Books')
+    name = fields.Char('Name', required=True)
+    birth_date = fields.Date('Birth date')
+    death_date = fields.Date('Death date')
+    gender = fields.Selection([('man', 'Man'), ('woman', 'Woman')], 'Gender')
 
 
 class Book(ModelSQL, ModelView):
@@ -49,10 +58,18 @@ class Book(ModelSQL, ModelView):
         ondelete='CASCADE')
     exemplaries = fields.One2Many('library.book.exemplary', 'book',
         'Exemplaries')
+    title = fields.Char('Title', required=True)
     genre = fields.Many2One('library.genre', 'Genre', ondelete='RESTRICT',
         required=False)
     editor = fields.Many2One('library.editor', 'Editor', ondelete='RESTRICT',
         required=True)
+    description = fields.Char('Description')
+    summary = fields.Text('Summary')
+    cover = fields.Binary('Cover')
+    page_count = fields.Integer('Page Count',
+        help='The number of page in the book')
+    edition_stopped = fields.Boolean('Edition stopped',
+        help='If True, this book will not be printed again in this version')
 
 
 class Exemplary(ModelSQL, ModelView):
@@ -61,3 +78,6 @@ class Exemplary(ModelSQL, ModelView):
 
     book = fields.Many2One('library.book', 'Book', ondelete='CASCADE',
         required=True)
+    identifier = fields.Char('Identifier', required=True)
+    acquisition_date = fields.Date('Acquisition Date')
+    acquisition_price = fields.Numeric('Acquisition Price', digits=(16, 2))

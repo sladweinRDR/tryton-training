8f36fcfae6e54c28aca4b3be0400e58f92e9564c
Samuel Ladwein <sladwein@roederer.fr>
2022-12-08 10:38:25 +0100

    feat(): fini step by step tuto for step5

--- modules/library/library.py
+++ modules/library/library.py
@@ -1,85 +1,130 @@
+from sql.aggregate import Count
 from trytond.model import ModelSQL, ModelView, fields
-
+from trytond.pool import Pool
+from trytond.transaction import Transaction
+import datetime
 
 __all__ = [
-    'Genre',
-    'Editor',
-    'EditorGenreRelation',
-    'Author',
-    'Book',
-    'Exemplary',
-    ]
+    "Genre",
+    "Editor",
+    "EditorGenreRelation",
+    "Author",
+    "Book",
+    "Exemplary",
+]
 
 
 class Genre(ModelSQL, ModelView):
-    'Genre'
-    __name__ = 'library.genre'
+    "Genre"
+    __name__ = "library.genre"
 
-    name = fields.Char('Name', required=True)
+    name = fields.Char("Name", required=True)
 
 
 class Editor(ModelSQL, ModelView):
-    'Editor'
-    __name__ = 'library.editor'
+    "Editor"
+    __name__ = "library.editor"
 
-    name = fields.Char('Name', required=True)
-    creation_date = fields.Date('Creation date',
-        help='The date at which the editor was created')
-    genres = fields.Many2Many('library.editor-library.genre', 'editor',
-        'genre', 'Genres')
+    name = fields.Char("Name", required=True)
+    creation_date = fields.Date(
+        "Creation date", help="The date at which the editor was created"
+    )
+    genres = fields.Many2Many(
+        "library.editor-library.genre", "editor", "genre", "Genres"
+    )
 
 
 class EditorGenreRelation(ModelSQL):
-    'Editor - Genre relation'
-    __name__ = 'library.editor-library.genre'
+    "Editor - Genre relation"
+    __name__ = "library.editor-library.genre"
 
-    editor = fields.Many2One('library.editor', 'Editor', required=True,
-        ondelete='CASCADE')
-    genre = fields.Many2One('library.genre', 'Genre', required=True,
-        ondelete='RESTRICT')
+    editor = fields.Many2One(
+        "library.editor", "Editor", required=True, ondelete="CASCADE"
+    )
+    genre = fields.Many2One(
+        "library.genre", "Genre", required=True, ondelete="RESTRICT"
+    )
 
 
 class Author(ModelSQL, ModelView):
-    'Author'
-    __name__ = 'library.author'
+    "Author"
+    __name__ = "library.author"
 
-    books = fields.One2Many('library.book', 'author', 'Books')
-    name = fields.Char('Name', required=True)
-    birth_date = fields.Date('Birth date')
-    death_date = fields.Date('Death date')
-    gender = fields.Selection([('man', 'Man'), ('woman', 'Woman')], 'Gender')
+    books = fields.One2Many("library.book", "author", "Books")
+    name = fields.Char("Name", required=True)
+    birth_date = fields.Date("Birth date")
+    death_date = fields.Date("Death date")
+    gender = fields.Selection([("man", "Man"), ("woman", "Woman")], "Gender")
+    age = fields.Function(fields.Integer("Age"), "getter_age")
+    number_of_books = fields.Function(
+        fields.Integer("Number of books"), "getter_number_of_books"
+    )
+
+    def getter_age(self, name):
+        if not self.birth_date:
+            return None
+        end_date = self.death_date or datetime.date.today()
+        age = end_date.year - self.birth_date.year
+
+        if (end_date, end_date.day) < (self.birth_date.month, self.birth_date.day):
+            age -= 1
+        return age
+
+    @classmethod
+    def getter_number_of_books(cls, authors, name):
+        result = {x.id: 0 for x in authors}
+        Book = Pool().get("library.book")
+        book = Book.__table__()
+
+        cursor = Transaction().connection.cursor()
+        cursor.execute(
+            *book.select(
+                book.author,
+                Count(book.id),
+                where=book.author.in_([x.id for x in authors]),
+                group_by=[book.author],
+            )
+        )
+        for author_id, count in cursor.fetchall():
+            result[author_id] = count
+        return result
 
 
 class Book(ModelSQL, ModelView):
-    'Book'
-    __name__ = 'library.book'
-    _rec_name = 'title'
+    "Book"
+    __name__ = "library.book"
+    _rec_name = "title"
 
-    author = fields.Many2One('library.author', 'Author', required=True,
-        ondelete='CASCADE')
-    exemplaries = fields.One2Many('library.book.exemplary', 'book',
-        'Exemplaries')
-    title = fields.Char('Title', required=True)
-    genre = fields.Many2One('library.genre', 'Genre', ondelete='RESTRICT',
-        required=False)
-    editor = fields.Many2One('library.editor', 'Editor', ondelete='RESTRICT',
-        required=True)
-    description = fields.Char('Description')
-    summary = fields.Text('Summary')
-    cover = fields.Binary('Cover')
-    page_count = fields.Integer('Page Count',
-        help='The number of page in the book')
-    edition_stopped = fields.Boolean('Edition stopped',
-        help='If True, this book will not be printed again in this version')
+    author = fields.Many2One(
+        "library.author", "Author", required=True, ondelete="CASCADE"
+    )
+    exemplaries = fields.One2Many("library.book.exemplary", "book", "Exemplaries")
+    title = fields.Char("Title", required=True)
+    genre = fields.Many2One(
+        "library.genre", "Genre", ondelete="RESTRICT", required=False
+    )
+    editor = fields.Many2One(
+        "library.editor", "Editor", ondelete="RESTRICT", required=True
+    )
+    description = fields.Char("Description")
+    summary = fields.Text("Summary")
+    cover = fields.Binary("Cover")
+    page_count = fields.Integer("Page Count", help="The number of page in the book")
+    edition_stopped = fields.Boolean(
+        "Edition stopped",
+        help="If True, this book will not be printed again in this version",
+    )
 
 
 class Exemplary(ModelSQL, ModelView):
-    'Exemplary'
-    __name__ = 'library.book.exemplary'
-    _rec_name = 'identifier'
+    "Exemplary"
+    __name__ = "library.book.exemplary"
+    _rec_name = "identifier"
 
-    book = fields.Many2One('library.book', 'Book', ondelete='CASCADE',
-        required=True)
-    identifier = fields.Char('Identifier', required=True)
-    acquisition_date = fields.Date('Acquisition Date')
-    acquisition_price = fields.Numeric('Acquisition Price', digits=(16, 2))
+    book = fields.Many2One("library.book", "Book", ondelete="CASCADE", required=True)
+    identifier = fields.Char("Identifier", required=True)
+    acquisition_date = fields.Date("Acquisition Date")
+    acquisition_price = fields.Numeric("Acquisition Price", digits=(16, 2))
+
+    def get_rec_name(self, name):
+        return '%s: %s' % (self.book.rec_name, self.identifier)

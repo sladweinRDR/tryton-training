6d80ff41fd7f66dffe5f808d1187e3def49ed194
Samuel Ladwein <sladwein@roederer.fr>
2022-12-13 13:57:05 +0100

    feat(): register all model

--- modules/library_borrow/__init__.py
+++ modules/library_borrow/__init__.py
@@ -1,7 +1,19 @@
 from trytond.pool import Pool
 
+from . import library
+from . import wizard
 
 def register():
-    Pool.register(module="library", type_="model")
+    Pool.register(
+        library.User,
+        library.Checkout,
+        library.Book,
+        library.Exemplary,
+        wizard.BorrowSelectBooks,
+        wizard.ReturnSelectCheckouts,
+        module="library", type_="model")
 
-    Pool.register(module="library", type_="wizard")
+    Pool.register(
+        wizard.Borrow,
+        wizard.Return,
+        module="library", type_="wizard")

b1bd0f40ab019733d56eeb183633e1071aa038b6
Samuel Ladwein <sladwein@roederer.fr>
2022-12-13 13:30:49 +0100

    fix(): PEP8 format

--- modules/library_borrow/__init__.py
+++ modules/library_borrow/__init__.py
@@ -1,8 +1,7 @@
 from trytond.pool import Pool
 
-def register():
-    Pool.register(
-        module='library', type_='model')
 
-    Pool.register(
-        module='library', type_='wizard')
+def register():
+    Pool.register(module="library", type_="model")
+
+    Pool.register(module="library", type_="wizard")
